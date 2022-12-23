155396e3cb9e2decc8c004ce6aaf1a8f6c383f23
VladyslavPrystupa <prystupa.vladyslavv@gmail.com>
2022-12-23 17:53:03 +0200

    task 2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,3 +1,20 @@
 import Player from '@vimeo/player';
+const throttle = require('lodash.throttle');
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
 
-console.log(Player)
+const KEY = "videoplayer-current-time"
+
+player.on('timeupdate', throttle(onPlay, 1000));
+    
+function onPlay(data) {
+    const seconds = Number.parseInt(data.seconds)
+    console.log(seconds)
+    const current = localStorage.setItem(KEY, seconds)
+    // player.setCurrentTime(current)
+}
+const result = localStorage.getItem(KEY)
+
+// console.log(result)
+
+player.setCurrentTime(result)
