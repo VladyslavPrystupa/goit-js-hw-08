2c284f4bb1b826cd4cc9ce8036c3f948540b1ae3
VladyslavPrystupa <prystupa.vladyslavv@gmail.com>
2022-12-23 12:31:44 +0200

    1task

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,7 +1,24 @@
 import SimpleLightbox from "simplelightbox";
 import "simplelightbox/dist/simple-lightbox.min.css";
+
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(SimpleLightbox);
+const gallery = document.querySelector('.gallery')
+
+const markup = galleryItems.map(({preview, original, description }) =>
+`<div class="gallery__item">
+<a class="gallery__item" href="${original}">
+  <img 
+  class="gallery__image" 
+  src="${preview}" 
+  alt="${description}"
+ />
+</a>
+</div>`).join("")
+
+gallery.innerHTML = markup
+
+
+const lightbox = new SimpleLightbox('.gallery a', { captionsData: 'alt', captionDelay: 250 });
